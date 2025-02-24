407c5bba9a24a17e2f6376b9f1bb444edcc65361
sg296858 <123054241+sg296858@users.noreply.github.com>
2025-02-24 19:27:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+
+
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
+
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    groq_api_key=st.text_input("Groq API Key",value="",type="password")
+
+generic_url=st.text_input("URL",label_visibility="collapsed")
+
+## Gemma Model USsing Groq API
+llm =ChatGroq(model="deepseek-r1-distill-qwen-32b", groq_api_key=groq_api_key)
+
+prompt_template="""
+Provide a summary of the following content in 300 words:
+Content:{text}
+
+"""
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
+
+if st.button("Summarize the Content from YT or Website"):
+    ## Validate all the inputs
+    if not groq_api_key.strip():
+        st.error("Please provide the information to get started")
+    else:
+        try:
+            with st.spinner("Waiting..."):
+                ## loading the website or yt video data
+                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
+                                                 headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                    docs=loader.load()
+
+                ## Chain For Summarization
+                    chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+                    output_summary=chain.run(docs)
+
+                    st.success(output_summary)
+        except Exception as e:
+            st.exception(f"Exception:{e}")
+                    
